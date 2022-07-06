158eff68f50a528879707bdd8524299dc3de9e87
ianchen0419 <ianchen0419@gmail.com>
2022-07-06 13:20:03 +0800

    upload

--- src/index.js
+++ src/index.js
@@ -4,10 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+import "@aws-amplify/ui-react/styles.css";
+import { HeroLayout1 } from './ui-components';
+
+import { Flex, View } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsExports);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HeroLayout1 />
+    
+    <Flex direction={{ base: 'column', large: 'row' }}>
+      <View width="100%" backgroundColor={{ base: 'orange', large: 'yellow' }}>
+      </View>
+      <View width="100%" backgroundColor={['orange', 'orange', 'orange', 'yellow']}>
+        there!
+      </View>
+    </Flex>
   </React.StrictMode>
 );
 
