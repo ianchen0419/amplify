80b690839bb993df6d9512227b0dee869e732e28
Ogriceboy <haoanhtran7@gmail.com>
2022-07-19 23:57:42 -0700

    added NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { NavBar } from './ui-components'
+import NavBar from './ui-components/NavBar'
 import Home from './pages/Home'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
