64de3dd75ce3dab83c4599673d74cdf660dde9aa
Ogriceboy <haoanhtran7@gmail.com>
2022-07-19 23:58:05 -0700

    Added ui-components to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
-
+import Hero2 from '../ui-components/HeroLayout2'
+import Features4x1 from '../ui-components/Features4x1'
+import Features2x2  from '../ui-components/Features2x2'
+import TallCard  from '../ui-components/TallCard'
 
 const Home = () => {
   return (
     <div>
-        Hello World
+        <Hero2 />
+        <Features4x1 />
+        <TallCard />
+        <TallCard />
+        <TallCard />
+        <TallCard />
+        <Features2x2 />
     </div>
   )
 }
