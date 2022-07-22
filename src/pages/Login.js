db38bb60b9db2d9c159d88117bdeca29d1fda64f
Ogriceboy <haoanhtran7@gmail.com>
2022-07-22 14:29:25 -0700

    update login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import LoginButton from '../components/LoginButton'
 import LogoutButton from '../components/LogoutButton'
+import { View } from '@aws-amplify/ui-react'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Login = () => {
+  const { isAuthenticated } = useAuth0()
+
   return (
-    <div>
-        <LoginButton />
-        <LogoutButton />
-    </div>
+    <View
+      as="div"
+      display="flex"
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+    </View>
   )
 }
 
